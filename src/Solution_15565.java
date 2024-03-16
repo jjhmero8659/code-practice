dcf9844c60e3e0ca45b5a463c637acabc4f5b2b6
jjhmero8659 <wkd86591@naver.com>
2024-03-16 22:41:12 +0900

    coding test practice

---
+++ src/Solution_15565.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+
+public class Solution_15565 {
+    static int N,minNum;
+    static int[] doll; //인형
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer stF = new StringTokenizer(br.readLine()," ");
+
+
+        N = Integer.parseInt(stF.nextToken());
+        minNum = Integer.parseInt(stF.nextToken());
+        doll = new int[N+2];
+
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        for (int i=1; i<=N; i++){
+            doll[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        int begin = 1;
+        int end = 1;
+
+        int lion = doll[1] == 1 ? 1 : 0; //첫번째 index 의 인형이 lion이면 1
+        int minLen = Integer.MAX_VALUE;
+
+
+
+
+        while (end <= N){
+            if (lion >= minNum){
+                minLen = Math.min(minLen , end - begin + 1);
+                if (doll[begin] == 1){
+                    lion--;
+                }
+                begin++;
+            }
+            else{
+                end++;
+                if (doll[end] == 1){
+                    lion++;
+                }
+            }
+        }
+
+
+        if (minLen == Integer.MAX_VALUE){
+            bw.write(-1+"\n");
+        }
+        else {
+            bw.write(minLen+"\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+
+
+}
---
+++ src/Solution_21318.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+
+public class Solution_21318 {
+    static int N,K;
+    static int[] score; //악보
+    static int[] mistake; // 실수 합 배열
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        N = Integer.parseInt(br.readLine());
+        score = new int[N+1];
+        mistake = new int[N+1];
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        
+        for (int i=1; i<=N; i++){
+            score[i] = Integer.parseInt(st.nextToken());
+
+        }
+
+        for (int i=2; i<=N; i++){ // 두번째 악보부터 마지막 까지
+            if (score[i] < score[i-1]){
+                mistake[i] = mistake[i-1] + 1;
+            }
+            else {
+                mistake[i] = mistake[i-1];
+            }
+        }
+
+
+        K = Integer.parseInt(br.readLine());
+        for (int i=1; i<=K; i++){
+            StringTokenizer stD = new StringTokenizer(br.readLine(), " ");
+            int result = solution(Integer.parseInt(stD.nextToken()) , Integer.parseInt(stD.nextToken()));
+            bw.write(result + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+
+    static int solution(int begin , int end){
+        return mistake[end] - mistake[begin];
+    }
+}
