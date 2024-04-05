9de60106f02cbc166918f109b0d8b708a2c2acfd
jjhmero8659 <wkd86591@naver.com>
2024-04-06 00:44:57 +0900

    coding test practice 2024-04-06

--- src/BFS/Solution_20058.java
+++ src/BFS/Solution_20058.java
@@ -1,50 +1,145 @@
 package BFS;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 
 public class Solution_20058 {
-    static int L,Q;
-    static int pow;
+    static int N, Q;
+    static int[] L;
     static int[][] map;
+    static int[] dx = {0, 1, 0, -1}; //동 남 서 북 x좌표
+    static int[] dy = {1, 0, -1, 0}; //동 남 서 북 y좌표
+    static boolean[][] visited;
+    static long maxIce = 0;
+    static long maxLand = 0;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        L = Integer.parseInt(st.nextToken()); //단계 횟수
+        N = Integer.parseInt(st.nextToken()); //N
         Q = Integer.parseInt(st.nextToken()); //시전 횟수
+        N = (int)Math.pow(2,N);
 
-        pow = (int)Math.pow(2,L);
+        map = new int[N][N];
+        visited = new boolean[N][N];
 
-        map = new int[pow][pow];
-
-        for (int i=0; i<pow; i++){
-            StringTokenizer stD = new StringTokenizer(br.readLine()," ");
-            for (int j=0; j<pow; j++){
+        for (int i = 0; i < N; i++) {
+            StringTokenizer stD = new StringTokenizer(br.readLine(), " ");
+            for (int j = 0; j < N; j++) {
                 map[i][j] = Integer.parseInt(stD.nextToken());
             }
         }
 
+        L = new int[Q];
+        StringTokenizer stL = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < Q; i++) {
+            L[i] = Integer.parseInt(stL.nextToken());
+        }
 
+        for (int i = 0; i < Q; i++) {
+            map = divide(L[i]);
+            map = melt();
+        }
 
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                maxIce += map[i][j];
+                if (map[i][j] != 0 && visited[i][j] == false){ //얼음 영역 이라면
+                    bfs(i,j);
+                }
+            }
+        }
 
-
+        bw.write(maxIce + "\n");
+        bw.write(maxLand + "\n");
         bw.flush();
         bw.close();
     }
 
-    static void areaDivide(){
-        int[][] replaceArea = new int[pow][pow];
+    static int[][] divide(int l) { //구역을 나누기
+        int[][] replaceArea = new int[N][N];
+        int powL = (int) Math.pow(2, l); // 2^l 으로 제곱 연산
+
+        for (int i = 0; i < N; i += powL) {
+            for (int j = 0; j < N; j += powL) {
+                rotateIce(i, j, powL, replaceArea);
+            }
+        }
+        return replaceArea;
+    }
+
+    static void rotateIce(int x, int y, int powL, int[][] replaceArea) {
+        for (int i = 0; i < powL; i++) {
+            for (int j = 0; j < powL; j++) {
+                replaceArea[j + x][powL - 1 - i + y] = map[x + i][y + j];
+            }
+        }
+    }
+
+    static int[][] melt() {
+        int[][] replaceArea = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            replaceArea[i] = Arrays.copyOf(map[i], N);
+        }
+
+        for (int x = 0; x < N; x++) {
+            for (int y = 0; y < N; y++) {
+                int cnt = 0;
+
+                if (map[x][y] == 0) {
+                    continue; //이미 얼음이 다녹은 칸
+                }
+
+                for (int i = 0; i < 4; i++) {
+                    int nx = x + dx[i];
+                    int ny = y + dy[i];
+
+                    if (0 <= nx && nx < N && 0 <= ny && ny < N) {
+                        if (map[nx][ny] > 0) {
+                            cnt++; //얼음이 주변에 있음
+                        }
+                    }
+                }
+
+                if (cnt < 3) {
+                    replaceArea[x][y]--; //임시 배열 얼음 값 감소
+                    //주변 3면 이상이 얼음 영역 아님
+                }
+            }
+        }
+        return replaceArea;
 
     }
-    static void bfs(int start) {
 
+    static void bfs(int x,int y) {
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[] {x,y});
+
+        visited[x][y] = true;
+        long land = 1;
+
+        while (!q.isEmpty()){
+            int[] now = q.poll();
+
+            for (int i=0; i<4; i++){
+                int nx = now[0] + dx[i];
+                int ny = now[1] + dy[i];
+
+                if (0 <= nx && nx < N && 0 <= ny && ny < N) {
+                    if (visited[nx][ny] == false && map[nx][ny] > 0){
+                        land++;
+                        visited[nx][ny] = true;
+                        q.offer(new int[] {nx,ny});
+                    }
+                }
+            }
+        }
+        maxLand = Math.max(land,maxLand);
     }
 
 }
--- src/test.java
+++ src/test.java
@@ -4,42 +4,128 @@ import java.util.function.Function;
 
 
 public class test {
-    static ArrayList<Integer> list;
-    static boolean[] visited;
-    static int[] num;
+    public static int n, q;
+    public static int[][] map;
+    public static int[] dx = {-1, 1, 0, 0};
+    public static int[] dy = {0, 0, -1, 1};
+    public static int land, totalIce;
 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
 
-        //n개의 정수를 입력받는다.
-        int n = scan.nextInt();
-        num = new int[n + 1];
-        for(int i = 1; i <= n; i++) {
-            num[i] = scan.nextInt();
+        n = Integer.parseInt(st.nextToken());
+        q = Integer.parseInt(st.nextToken());
+
+        n = (int) Math.pow(2, n);
+        map = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
         }
 
-        //순서대로 사이클이 발생하는지 dfs로 확인한다.
-        list = new ArrayList<>();
-        visited = new boolean[n + 1];
-        for(int i = 1; i <= n; i++) {
-            visited[i] = true;
-            dfs(i, i);
-            visited[i] = false;
+        st = new StringTokenizer(br.readLine());
+        int[] L = new int[q];
+        for (int i = 0; i < q; i++) {
+            L[i] = Integer.parseInt(st.nextToken());
         }
 
-        Collections.sort(list); //작은 수 부터 출력하므로 정렬한다.
-        System.out.println(list.size());
-        for(int i = 0; i < list.size(); i++) {
-            System.out.println(list.get(i));
+        for (int i = 0; i < q; i++) {
+            // 파이어스톰 시전
+            map = divide(L[i]);// 회전
+            map = melt();// 얼음 녹이기
+        }
+        land = totalIce = 0;
+
+        biggest();
+        System.out.println(totalIce);
+        System.out.println(land);
+
+    }
+
+    public static int[][] divide(int L) {
+        int[][] tmp = new int[n][n];
+        L = (int) Math.pow(2, L);
+        for (int i = 0; i < n; i += L) {
+            for (int j = 0; j < n; j += L) {
+                rotate(i, j, L, tmp);
+            }
+        }
+        return tmp;
+
+    }
+
+    public static void rotate(int x, int y, int L, int[][] tmp) {
+        for (int i = 0; i < L; i++) {
+            for (int j = 0; j < L; j++) {
+                tmp[x + i][y + j] = map[x + L - 1 - j][y + i];
+            }
         }
     }
 
-    public static void dfs(int start, int target) {
-        if(visited[num[start]] == false) {
-            visited[num[start]] = true;
-            dfs(num[start], target);
-            visited[num[start]] = false;
+    public static int[][] melt() {
+        int[][] tmp = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            tmp[i] = Arrays.copyOf(map[i], n);
         }
-        if(num[start] == target) list.add(target); //사이클 발생시 해당 숫자를 list에 담아준다.
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int cnt = 0;
+                if (map[i][j] == 0)
+                    continue;
+                for (int k = 0; k < 4; k++) {
+                    int nx = i + dx[k];
+                    int ny = j + dy[k];
+                    if (nx >= 0 && nx < n && ny >= 0 && ny < n) {
+                        if (map[nx][ny] > 0) {
+                            cnt++;
+                        }
+                    }
+                }
+                if (cnt < 3)
+                    tmp[i][j]--;
+            }
+        }
+        return tmp;
+    }
+
+    public static void biggest() {
+        Queue<int[]> q = new LinkedList<>();
+        boolean[][] visit = new boolean[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                totalIce += map[i][j];
+                if (map[i][j] > 0 && !visit[i][j]) {
+                    q.add(new int[]{i, j});
+                    visit[i][j] = true;
+                    int cnt = 1;
+
+                    while (!q.isEmpty()) {
+                        int[] t = q.poll();
+                        int tx = t[0];
+                        int ty = t[1];
+
+                        for (int k = 0; k < 4; k++) {
+                            int nx = tx + dx[k];
+                            int ny = ty + dy[k];
+                            if (nx >= 0 && nx < n && ny >= 0 && ny < n) {
+                                if (map[nx][ny] > 0 && !visit[nx][ny]) {
+                                    visit[nx][ny] = true;
+                                    q.add(new int[]{nx, ny});
+                                    cnt++;
+                                }
+                            }
+                        }
+
+                    }
+                    land = Math.max(land, cnt);
+                }
+            }
+        }
     }
 }
