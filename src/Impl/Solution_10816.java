045f9d7a427b979c589e96c065ef0608f7b1379f
jjhmero8659 <wkd86591@naver.com>
2024-04-08 00:43:33 +0900

    coding test practice 2024-04-08

--- src/Impl/Solution_10816.java
+++ src/Impl/Solution_10816.java
@@ -35,43 +35,41 @@ public class Solution_10816 {
         StringTokenizer stD = new StringTokenizer(br.readLine(), " ");
 
         for (int i = 1; i <= M; i++) {
-            int findNum = Integer.parseInt(stD.nextToken());
-
-            int find = bst(findNum);
-            bw.write(find == -1 ? "0 " : find + " ");
+            int targetNum = Integer.parseInt(stD.nextToken());
+            int uC = upperBound(targetNum);
+            int lC = lowerBound(targetNum);
+            bw.write(uC - lC + " ");
         }
 
         bw.flush();
         bw.close();
     }
 
-    static int bst(int targetNum){
-        int startIdx = 0;
-        int endIdx = num.length - 1;
-
-        while (startIdx <= endIdx){
-            int midIdx = (startIdx + endIdx) / 2;
-            int midVal = num[midIdx];
-
-            if (midVal < targetNum){
-                startIdx = midIdx + 1;
-            }
-            else if(midVal > targetNum){
-                endIdx = midIdx - 1;
-            }
-            else{
-                int cnt = 1;
-                while (true){
-                    midIdx++; //다음 숫자
-                    if (num[midIdx] != midVal){
-                        break;
-                    }
-                    cnt++;
-                }
-                return cnt; // index 반환
+    static int lowerBound(int target) {
+        int low = 0;
+        int high = num.length;
+        while (low < high) {
+            int mid = (high + low) / 2;
+            if (target <= num[mid]) {
+                high = mid;
+            } else {
+                low = mid + 1;
             }
         }
-        return -1;
+        return low;
     }
 
+    static int upperBound(int target) {
+        int low = 0;
+        int high = num.length;
+        while (low < high) {
+            int mid = (high + low) / 2;
+            if (target < num[mid]) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
 }
