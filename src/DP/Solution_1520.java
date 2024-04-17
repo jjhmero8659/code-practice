a32a513f9539fefe0441641e52c3b915f8e8a22b
jjhmero8659 <wkd86591@naver.com>
2024-04-18 01:39:17 +0900

    coding test practice 2024-04-18

--- src/DP/Solution_1520.java
+++ src/DP/Solution_1520.java
@@ -8,7 +8,6 @@ public class Solution_1520 {
     static int N, M;
     static long[][] dp;
     static long[][] map;
-    static boolean visited[][];
     static int[] dx = {0, 1, 0, -1}; // 동 남 서 북
     static int[] dy = {1, 0, -1, 0}; // 동 남 서 북
 
@@ -22,48 +21,44 @@ public class Solution_1520 {
 
         dp = new long[N][M]; // 해당 영역 도착 경로 개수
         map = new long[N][M];
-        visited = new boolean[N][M];
 
 
         for (int i = 0; i < N; i++) {
             StringTokenizer stD = new StringTokenizer(br.readLine(), " ");
-            for (int j = 0; j < N; j++) {
+            for (int j = 0; j < M; j++) {
                 map[i][j] = Long.parseLong(stD.nextToken());
+                dp[i][j] = -1;
             }
         }
 
-        dp[0][0] = 1; // (0,0) 에서 (0,0)으로 도착하는 default = 1
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (i == N - 1 && j == N - 1) {
-                    break;
-                }
-
-                if (dp[i][j] > 0) { //해당 영역에 도달하는 경우의 수가 있다면
-                    // 이후 탐색이 가능하다.
-                    long nowH = map[i][j]; //현재 높이
-
-                    for (int h = 0; h < 4; h++) {
-                        int nx = i + dx[h];
-                        int ny = j + dy[h];
-
-                        if (0 <= nx && nx < N && 0 <= ny && ny < N) {
-                            if (nowH > map[nx][ny]){
-                                dp[nx][ny] += dp[i][j];
-                                visited[nx][ny] = true;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        bw.write(dp[N - 1][N - 1] + "\n");
+        bw.write(dfs(0, 0) + "\n");
         bw.flush();
         bw.close();
         br.close();
     }
 
+    static long dfs(int x, int y) {
+        if (x == N - 1 && y == M - 1) {
+            return 1; //도달
+        }
+
+        //방문 기록이 없다면
+        if (dp[x][y] == -1) {
+            dp[x][y] = 0; //현재 영역을 0 초기화
+
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i]; //다음 x
+                int ny = y + dy[i]; //다음 y
+
+                if (0 <= nx && nx < N && 0 <= ny && ny < M) {
+                    if (map[x][y] > map[nx][ny]){
+                        dp[x][y] += dfs(nx,ny);
+                    }
+                }
+            }
+        }
+
+        return dp[x][y];
+    }
 
 }
--- src/test.java
+++ src/test.java
@@ -4,134 +4,58 @@ import java.util.function.Function;
 
 
 public class test {
-    static int N, M;
-    static int map[][];
-    static boolean visited[][];
-    static int[] dx = {0, 1, 0, -1}; //동 남 서 북 x
-    static int[] dy = {1, 0, -1, 0}; //동 남 서 북 y
-    static int maxSafeCnt = 0;
+    static int M, N;
 
-    static class Virus {
-        int x;
-        int y;
-
-        Virus(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
-
+    static int[][] map;
+    static int[][] dp;
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, -1, 0, 1};
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        M = Integer.parseInt(st.nextToken()); // 세로
+        N = Integer.parseInt(st.nextToken()); // 가로
 
-        N = Integer.parseInt(st.nextToken()); //세로
-        M = Integer.parseInt(st.nextToken()); //가로
+        map = new int[M][N];
+        dp = new int[M][N];
 
-        visited = new boolean[N][M];
-        map = new int[N][M];
-
-        for (int i = 0; i < N; i++) {
-            StringTokenizer stD = new StringTokenizer(br.readLine(), " ");
-            for (int j = 0; j < M; j++) {
-                map[i][j] = Integer.parseInt(stD.nextToken());
+        for (int i=0; i<M; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j=0; j<N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+                dp[i][j] = -1; // dp 초기화
             }
         }
 
-        dfs(0);
+        int dfs = dfs(0, 0);
 
-        bw.write(maxSafeCnt + "\n");
-        bw.flush();
-        bw.close();
+        System.out.println(
+
+        ); // 출발 지점
     }
 
-    static void dfs(int depth) {
-        if (depth == 3) {
-            bfs(); //벽이 완성 된 상태의 바이러스 안전지대 확인
-            return;
-        }
+    public static int dfs(int x, int y) {
+        // 도착지점까지 도달했을 경우
+        if (x == M-1 && y == N-1)
+            return 1;
 
-//        for (int i = 0; i < N; i++) {
-//            for (int j = 0; j < M; j++) {
-//                if (map[i][j] == 0) { //벽을 세울 수 있는 빈 공간 이라면
-//                    map[i][j] = 1; //현재 위치에 벽을 세우고
-//                    dfs(depth + 1); //depth 1 추가 후 재귀
-//                    map[i][j] = 0; //현재 위치를 다시 빈공간으로 복귀
-//                    //이어서 반복분
-//                }
-//            }
-//        }
+        // 방문한 적이 없는 경우
+        if (dp[x][y] == -1) {
+            dp[x][y] = 0;
+            for (int i=0; i<4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
 
-        for (int i=0; i < (N * M); i++){
-            if (map[i/M][i%M] == 0){
-                map[i/M][i%M] = 1;
-                dfs(depth + 1); //depth 1 추가 후 재귀
-                map[i/M][i%M] = 0;
-            }
-        }
-    }
+                if (nx < 0 || nx > M-1 || ny < 0 || ny > N-1) continue;
 
-    static void bfs() {
-        int[][] virusMap = new int[N][M]; //virus Map
-        //bfs 로 탐색하면서 내부 값을 변경 하기 때문에 임시 map 생성
-        boolean[][] visited = new boolean[N][M]; //방문
-
-        Queue<Virus> q = new ArrayDeque<>();
-
-        for (int i = 0; i < N; i++) { //맵 복사
-            for (int j = 0; j < M; j++) {
-                virusMap[i][j] = map[i][j];
-                if (map[i][j] == 2) { //virus 영역 이라면
-                    q.offer(new Virus(i, j)); //큐에 추가
-                    visited[i][j] = true;
+                // 내리막 길인 경우
+                if (map[x][y] > map[nx][ny]) {
+                    dp[x][y] += dfs(nx, ny);
                 }
             }
         }
-
-        //현재 까지 Queue 에 모든 virus 좌표 값이 저장 되어 있음
-
-        while (!q.isEmpty()) {
-            Virus virus = q.poll();
-            int cx = virus.x; //현재 x좌표
-            int cy = virus.y; //현재 y좌표
-
-            for (int i = 0; i < 4; i++) {
-                int nx = cx + dx[i]; //next x
-                int ny = cy + dy[i]; //next y
-
-                if (0 <= nx && nx < N && 0 <= ny && ny < M) {
-                    if (visited[nx][ny] == false && virusMap[nx][ny] == 0) { //방문하지 않은 상태 , 빈공간 이라면
-                        visited[nx][ny] = true; //다음 영역 visited true
-                        virusMap[nx][ny] = 2; //다음 영역을 virus로 변경
-                        q.offer(new Virus(nx, ny));
-                    }
-                }
-            }
-        }
-
-        //virus 전부 변환 후
-        //안전 영역 숫자 check
-        int safeCnt = checkSafeArea(virusMap);
-
-        maxSafeCnt = Math.max(safeCnt, maxSafeCnt);
+        return dp[x][y];
     }
-
-    static int checkSafeArea(int[][] virusMap) {
-        int safeCnt = 0;
-
-        for (int i = 0; i < N; i++) { //맵 복사
-            for (int j = 0; j < M; j++) {
-                if (virusMap[i][j] == 0) {
-                    safeCnt++;
-                }
-            }
-        }
-
-        return safeCnt;
-    }
-
 }
