4308874201563df5f958c40c9b459ecf5c4a8853
jjhmero8659 <wkd86591@naver.com>
2024-06-12 00:01:57 +0900

    coding test practice 2024-06-12

--- src/BackTracking/Solution_15658.java
+++ src/BackTracking/Solution_15658.java
@@ -10,7 +10,7 @@ public class Solution_15658 {
     static int N;
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static int[] inputArr, arr , operator;
+    static int[] inputArr, operator;
     static int maxValue = Integer.MIN_VALUE;
     static int minValue = Integer.MAX_VALUE;
 
@@ -19,33 +19,56 @@ public class Solution_15658 {
 
         N = Integer.parseInt(st.nextToken()); //숫자 N
 
-        inputArr = new int[N + 1];
-        arr = new int[N + 1];
+        inputArr = new int[N];
 
-        StringTokenizer stD = new StringTokenizer(br.readLine()," ");
-        for (int i = 1; i <= N; i++) {
+        StringTokenizer stD = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
             inputArr[i] = Integer.parseInt(stD.nextToken());
         }
 
         operator = new int[4];
-        StringTokenizer stO = new StringTokenizer(br.readLine()," ");
-        for (int i=0; i<4; i++){
+        StringTokenizer stO = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < 4; i++) {
             operator[i] = Integer.parseInt(stO.nextToken());
         }
 
-
-
-
+        backTracking(inputArr[0] , 1);
+        bw.write(maxValue + "\n");
+        bw.write(minValue + "\n");
         bw.flush();
         br.close();
         bw.close();
     }
 
     static void backTracking(int num, int depth) {
-        if (depth == N + 1){
-            maxValue = Math.max(num , maxValue);
-            minValue = Math.min(num , minValue);
+        if (depth == N) {
+            maxValue = Math.max(num, maxValue);
+            minValue = Math.min(num, minValue);
             return;
+        }
+
+        for (int i = 0; i < 4; i++) {
+            if (operator[i] > 0){
+                operator[i]--;
+
+                switch (i) {
+                    case 0:
+                        backTracking(num + inputArr[depth], depth + 1);
+                        break;
+                    case 1:
+                        backTracking(num - inputArr[depth], depth + 1);
+                        break;
+                    case 2:
+                        backTracking(num * inputArr[depth], depth + 1);
+                        break;
+                    case 3:
+                        backTracking(num / inputArr[depth], depth + 1);
+                        break;
+                }
+
+                operator[i]++;
+            }
+
         }
     }
 
